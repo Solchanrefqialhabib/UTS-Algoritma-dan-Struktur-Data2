534b1c270beaf7a438112ea1b8e4c2c523d48042
Solchan  Refqi  Al Habib <80452116+Solchanrefqialhabib@users.noreply.github.com>
2021-04-20 11:21:29 +0700

    Add files via upload

---
+++ Nomor1.java
@@ -0,0 +1,27 @@
+
+package utsamaster;
+
+public class Nomor1 {
+    
+    public static void main(String[] args){
+         int[] data = {3,10,4,2,8,13};
+        for (int i=0; i<data.length; i++){
+            System.out.print(data[i]+" ");
+        }
+        for (int i=1; i<data.length; i++){
+            int key = data[i];
+            int j = i-1;
+            while((j>=0)&&(data[j]>key)){
+                data[j+1] = data[j];
+                j--;
+            }
+            data[j+1] = key;
+        }
+        System.out.println("\nsorted by Solchan Refqi Al Habib:");
+        System.out.println("=== selection sort ===");
+        for (int i=0; i<data.length; i++){
+            System.out.print(data[i]+" ");
+        }
+    }
+}
+
